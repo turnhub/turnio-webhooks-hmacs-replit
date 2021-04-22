fd3d77603a1a86925536701888491cf1c11d3e4b
Shaun Sephton <shaun@sephton.io>
2021-04-22 11:10:55 +0000

    Add HMAC validation

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
-from flask import Flask
+from hashlib import sha256
+import base64
+import hmac
 import os
-import requests
+import codecs
+
+from flask import Flask, request
 
 app = Flask('app')
 
-TOKEN = os.environ.get("TOKEN")
+@app.route('/webhook', methods=["POST"])
+def webhook():
+    # Get HMAC and calculated by Turn from request header.
+    received_hmac = request.headers['X-Turn-Hook-Signature']
+
+    # Calculate an HMAC for the request payload using secret 
+    # as sourced from Turn.
+    secret = os.environ['SECRET']
+    h = hmac.new(codecs.encode(secret), request.data, sha256)
+    calculated_hmac = base64.b64encode(h.digest()).decode('ascii')
+
+    print(f'Received HMAC: {received_hmac}')
+    print(f'Calculated HMAC: {calculated_hmac}')
+
+    # If received and calculated HMAC matches proceed with processing
+    # webhook since it was in fact received from Turn. Otherwise abort 
+    # processing.
+    if received_hmac == calculated_hmac:
+      # ... do some things here ...
+      return "Valid HMAC received", 200
+    else:
+      return "Invalid HMAC received.", 403
 
 @app.route('/')
 def index():
   return 'The Turn Webhook API endpoint is at /webhook'
-
-@app.route('/webhook', methods=["POST"])
-def webhook():
-    from flask import request
-    json = request.json
-    return ""
 
 
 app.run(host='0.0.0.0', port=8080)
